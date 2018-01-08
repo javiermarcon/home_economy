c92736696d664eb433f1740231930e7c8760d8c6
Javier Marcon <javiermarcon@gmail.com>
2018-01-08 07:00:45 -0300

    grilla en mainscreen y sacado comentado outline kv

--- src/hegui/mainscreen.py
+++ src/hegui/mainscreen.py
@@ -2,8 +2,15 @@
 
 from kivy.app import App
 from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.recycleview import RecycleView
 
 class MainPanel(BoxLayout):
     def disconnect(self):
         app = App.get_running_app()
         app.do_logout()
+
+class RV(RecycleView):
+    """ver: https://kivy.org/docs/api-kivy.uix.recycleview.html#kivy.uix.recycleview.RecycleView"""
+    def __init__(self, **kwargs):
+        super(RV, self).__init__(**kwargs)
+        self.data = [{'text': str(x)} for x in range(100)]
