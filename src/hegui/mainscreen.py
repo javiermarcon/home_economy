ebdf5da7035cf41f4f5a0a1dcbcc9926a9f2b7ba
Javier Marcon <javiermarcon@gmail.com>
2018-01-15 00:29:22 -0300

    Cambiado recyclerview por treeview en mainscreen

--- src/hegui/mainscreen.py
+++ src/hegui/mainscreen.py
@@ -2,13 +2,31 @@
 
 from kivy.app import App
 from kivy.uix.boxlayout import BoxLayout
-from kivy.uix.recycleview import RecycleView
+from kivy.uix.treeview import TreeViewLabel
 
 class MainPanel(BoxLayout):
-    pass
 
-class RV(RecycleView):
-    """ver: https://kivy.org/docs/api-kivy.uix.recycleview.html#kivy.uix.recycleview.RecycleView"""
     def __init__(self, **kwargs):
-        super(RV, self).__init__(**kwargs)
-        self.data = [{'text': str(x)} for x in range(100)]
+        super(MainPanel, self).__init__(**kwargs)
+        mc = self.main_cuentas
+        self.populate_treeview(mc)
+        mc.bind(minimum_height=mc.setter('height'))
+
+    def populate_treeview(self, tv):
+        n = tv.add_node(TreeViewLabel(text='Item 1'))
+        for x in xrange(30):
+            tv.add_node(TreeViewLabel(text='Subitem %d' % x), n)
+        n = tv.add_node(TreeViewLabel(text='Item 2', is_open=True))
+        for x in xrange(30):
+            tv.add_node(TreeViewLabel(text='Subitem %d' % x), n)
+        n = tv.add_node(TreeViewLabel(text='Item 3'))
+        for x in xrange(30):
+            tv.add_node(TreeViewLabel(text='Subitem %d' % x), n)
+        return tv
+
+#class TV(TreeView):
+    #pass
+    #"""ver: https://kivy.org/docs/api-kivy.uix.recycleview.html#kivy.uix.recycleview.RecycleView"""
+    #def __init__(self, **kwargs):
+    #    super(RV, self).__init__(**kwargs)
+    #    self.data = [{'text': str(x)} for x in range(100)]
