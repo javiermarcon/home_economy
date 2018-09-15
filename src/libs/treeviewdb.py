805e40405d6cb9d06beb64a855d3f50bc802853e
Javier Marcon <javiermarcon@gmail.com>
2018-09-15 17:41:09 -0300

    treeviewdb with id en nodos y default_selected

--- src/libs/treeviewdb.py
+++ src/libs/treeviewdb.py
@@ -3,6 +3,12 @@
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.treeview import TreeViewLabel
 from util.recursive_attributes import rec_getattr
+from kivy.properties import StringProperty
+
+class IdTreeViewLabel(TreeViewLabel):
+
+    value = StringProperty()
+
 
 class TreeViewDb(BoxLayout):
 
@@ -15,18 +21,19 @@ class TreeViewDb(BoxLayout):
     dbchildargs = None
     dbchildtext = None
 
-    def populate_treeview(self, tv):
+    def populate_treeview(self, tv, selected_id=None):
         """
         Llena el tree view
         :param tv: kivy tree view
         :return: None
         """
+
         if self.dbparentclass:
             tipos_cuenta = self.dbparentclass.get_all()
             for act in tipos_cuenta:
                 act_params = [rec_getattr(act, attr) for attr in self.dbparentargs]
                 txt_act = self.dbparenttext.format(*act_params)
-                nodo = tv.add_node(TreeViewLabel(text=txt_act, is_open=True))
+                nodo = tv.add_node(IdTreeViewLabel(text=txt_act, value=act.id, is_open=True))
                 self.nodos_tipo[act.id] = nodo
         cuentas = self.dbchildclass.get_all()
 
@@ -35,15 +42,18 @@ class TreeViewDb(BoxLayout):
             txt_cuenta = self.dbchildtext.format(*c_params)
 
             if cuenta.parent:
-                nodo = tv.add_node(TreeViewLabel(text=txt_cuenta, is_open=True),
+                nodo = tv.add_node(IdTreeViewLabel(text=txt_cuenta, value=cuenta.id, is_open=True),
                                    self.nodos_cuenta[cuenta.parent])
             else:
                 if self.dbparentclass:
-                    nodo = tv.add_node(TreeViewLabel(text=txt_cuenta, is_open=True),
+                    nodo = tv.add_node(IdTreeViewLabel(text=txt_cuenta, value=cuenta.id, is_open=True),
                                        self.nodos_tipo[cuenta.acounttype.id])
                 else:
-                    nodo = tv.add_node(TreeViewLabel(text=txt_cuenta, is_open=True))
+                    nodo = tv.add_node(IdTreeViewLabel(text=txt_cuenta, value=cuenta.id, is_open=True))
             self.nodos_cuenta[cuenta.id] = nodo
+            print cuenta.id
+            if cuenta.id == selected_id:
+                tv.select_node(nodo)
         return tv
 
     def set_treeview_data(self, dbchildclass, dbchildargs, dbchildtext,
@@ -55,4 +65,3 @@ class TreeViewDb(BoxLayout):
             self.dbparentclass = dbparentclass()
             self.dbparentargs = dbparentargs
             self.dbparenttext = dbparenttext
-
