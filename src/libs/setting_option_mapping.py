42408c6be28b98ce4aaeaea3d695f928926c4c9f
Javier Marcon <javiermarcon@gmail.com>
2018-11-19 16:39:24 -0300

    Agregado scroll a las opciones de SettingOptionMapping

--- src/libs/setting_option_mapping.py
+++ src/libs/setting_option_mapping.py
@@ -1,18 +1,25 @@
-"""These options allow to show a different value than the value.
+# -*- coding: utf-8 -*-
 
-Most of this file is copied from :class:`kivy.uix.settings.SettingOptions`.
-
-This code was copied from https://github.com/fossasia/kniteditor
 """
+These options allow to show a different value than the value to handle
+id and text pairs.
+
+This code was a merge from kniteditor’s SettingOptionMapping
+(https://github.com/fossasia/kniteditor)
+and kivy SettingScrollOptions
+(https://github.com/kivy/kivy/wiki/Scollable-Options-in-Settings-panel)
+"""
+
 from kivy.properties import DictProperty, ObjectProperty
-from kivy.uix.settings import SettingItem, SettingsWithTabbedPanel
-from kivy.uix.boxlayout import BoxLayout
-from kivy.core.window import Window
+from kivy.uix.settings import SettingItem
 from kivy.uix.widget import Widget
 from kivy.uix.popup import Popup
 from kivy.uix.togglebutton import ToggleButton
 from kivy.metrics import dp
 from kivy.uix.button import Button
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.scrollview import ScrollView
+from kivy.uix.settings import SettingSpacer
 
 
 class SettingOptionMapping(SettingItem):
@@ -51,28 +58,32 @@ class SettingOptionMapping(SettingItem):
 
     def _create_popup(self, instance):
         # create the popup
-        content = BoxLayout(orientation='vertical', spacing='5dp')
-        popup_width = min(0.95 * Window.width, dp(500))
-        self.popup = popup = Popup(
-            content=content, title=self.title, size_hint=(None, None),
-            size=(popup_width, '400dp'))
-        popup.height = len(self.options) * dp(55) + dp(150)
+        content = GridLayout(cols=1, spacing='5dp')
+        scrollview = ScrollView(do_scroll_x=False)
+        scrollcontent = GridLayout(cols=1, spacing='5dp', size_hint=(None, None))
+        scrollcontent.bind(minimum_height=scrollcontent.setter('height'))
+        self.popup = popup = Popup(content=content, title=self.title, size_hint=(0.5, 0.9), auto_dismiss=False)
 
+        # we need to open the popup first to get the metrics
+        popup.open()
+        # Add some space on top
+        content.add_widget(Widget(size_hint_y=None, height=dp(2)))
         # add all the options
-        content.add_widget(Widget(size_hint_y=None, height=1))
         uid = str(self.uid)
-        for option, text in sorted(self.options.items(), key=lambda t: t[1]):
+        for option, text in self.options.items():
             state = 'down' if option == self.value else 'normal'
-            btn = ToggleButton(text=text, state=state, group=uid)
+            btn = ToggleButton(text=text, state=state, group=uid, size=(popup.width, dp(55)), size_hint=(None, None))
             btn.bind(on_release=lambda instance,
                      option=option: self._set_option(option))
-            content.add_widget(btn)
+            scrollcontent.add_widget(btn)
 
         # finally, add a cancel button to return on the previous panel
-        content.add_widget(Widget())  # SettingSpacer
-        btn = Button(text='Cancel', size_hint_y=None, height=dp(50))
+        scrollview.add_widget(scrollcontent)
+        content.add_widget(scrollview)
+        content.add_widget(SettingSpacer())
+        # btn = Button(text='Cancel', size=((oORCA.iAppWidth/2)-sp(25), dp(50)),size_hint=(None, None))
+        btn = Button(text='Cancel', size=(popup.width, dp(50)), size_hint=(0.9, None))
         btn.bind(on_release=popup.dismiss)
         content.add_widget(btn)
 
-        # and open the popup !
-        popup.open()
+
